e2ed9f7366a87cc83be37b310a2df19eadd3f4bc
fbouazza <fbouazza.pro@gmail.com>
2020-11-10 15:43:43 +0100

    Last commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Asynchrone_request
 Faire des requêtes en asynchrone avec NodeJS
+
+
+## Utilisation
+
+```
+npm start
+```
+
+Vous verrez ici lors des résultats que les retours de requête s'affichent par rapport à la rapidité de la réponse des serveurs  
+et non dans l'ordre du tableau 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const async_request = require("./utils/async.js")
 
-const tab = ["https://www.google.fr", "https://www.youtube.com", "https://www.lilleweb.fr", "https://www.bateauamorceur.com"]
+const tab = ["https://www.google.fr", "https://www.youtube.com", "https://github.com", "https://www.udemy.com", "https://www.pagesjaunes.fr/", "https://fr.wikipedia.org"]
 
 tab.forEach(url => {
   async_request(url, (error, response) => {
